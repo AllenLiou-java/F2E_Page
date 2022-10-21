f128d296dc3e87f38f6be7373f31ed23d1c9e762
AllenLiou-java <75025074+AllenLiou-java@users.noreply.github.com>
2022-10-21 17:58:44 +0800

    init project

---
+++ pages/about/index.vue
@@ -0,0 +1,30 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <nuxt-link to="/">Home</nuxt-link>
+    <h3>{{ fullName }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    data() {
+      return {
+        message: "Hello World",
+        firstName: "Allen",
+        lastName: "Liou"
+      }
+    },
+    computed: {
+      fullName(): string {
+        return `${this.firstName} ${this.lastName}`
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="container">
+    <div>
+      <h2>Home</h2>
+      <h2>
+        <nuxt-link to="/about">About</nuxt-link>
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
